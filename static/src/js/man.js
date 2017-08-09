c40e9a0da3871b29ebae7a96ef24640afe540e33
Pankaj Kamani <pam@odoo.com>
2017-08-09 18:31:28 +0530

    added transparent navbar

--- static/src/js/man.js
+++ static/src/js/man.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
-	// // dropdown animation
-	// $('.dropdown-toggle').click(function () {
- //    	$(this).next('.dropdown-menu').slideToggle(400);
-	// });
+	// dropdown animation
+	$('.dropdown-toggle').click(function () {
+    	$(this).next('.dropdown-menu').slideToggle(400);
+	});
 
-	// $('.dropdown-toggle').focusout(function () {
- //    	$(this).next('.dropdown-menu').slideUp(400);
-	// });
+	$('.dropdown').mouseleave(function () {
+    	$(this).next('.dropdown-menu').slideUp(400);
+	});
 
+	// scrollbar-function
+	$( window ).scroll(function() {
+		var height = $(window).scrollTop()
+		if (height >= 70){
+  			$( ".navbar" ).addClass('opaque');
+		}
+		else{
+  			$( ".navbar" ).removeClass('opaque');
+		}
+	});
 	// card hover animations
 	$('.hover').click(function(e){
 	    $( this ).addClass('flip');
