d1547149c493823f0ca65afba1b903ec171c4375
pankaj9099 <kamanipankaj9099@gmail.com>
2017-10-10 00:29:31 +0530

    [IMP] js : more improvements and modular script

--- static/src/js/main_article_try.js
+++ static/src/js/main_article_try.js
@@ -23,19 +23,21 @@ define(['jquery', 'underscore', 'pop_animate', 'cookie', 'index'], function($, _
             },
         });
 		var ajax_caller = (function(self, $){
-				var errorHtml = _.template("<ul><li>Error: <%= error %></li><li>Error Code<%= error_code %></li></ul>")  
+
+				var errorHtml = _.template("<ul><li class='error'>Error: <%= error %></li><li class='error'>Error Code: <%= error_code %></li></ul>")  
 		        var genericRpc = function(self){
 		            var request = $.ajax({
 		                url: $(self).attr('href'),
 		                type: self.method,
 		                data: {'article_id': $(self).attr('data-id'), 'user_id': $(self).attr('data-user')},
 		                dataType: 'json',
-		            });
-		            return request;
+		            })
+		            return resolver(request, self)
 		        };
 
-		        var makeCall = function(){
-
+		        var makeCall = function(event){
+    				var self = this	
+    				event.preventDefault()
 		        	if ($(this).hasClass('down')){
 		        		this.method = 'delete';
 		        	}
@@ -45,41 +47,42 @@ define(['jquery', 'underscore', 'pop_animate', 'cookie', 'index'], function($, _
 
 		        	return genericRpc(this)
 		        };
+		        var resolver = function(request, self){
+		        	request.fail(function(response){
+		        		
+			        	$('#popup1 .content').html(ajax_caller.errorHtml({error: response.responseJSON.error ||response.responseJSON.detail , error_code: response.responseJSON.error_code ||response.responseJSON.status_code}));
+			        	$('#popup1').show()
+			        });
+			        request.done(function(response) {
+			        	debugger;
+				        $(self).prev().val(response.total_likes);
+				        $(self).toggleClass('down');
+				        // $(self).find('i.fa').toggleClass('shake').toogleClass('rotateInUpRight');
+				        $(self).find('i.fa').toggleClass('fa-thumbs-o-up').toggleClass('fa-thumbs-up')
+				        if(this.type == 'DELETE'){
+				        	$(self).find('i.fa').removeClass('zoomIn').addClass('rotateInUpRight');
+				        }
+				        else{
+				        $(self).find('i.fa').removeClass('rotateInUpRight').addClass('zoomIn');
+
+				        }
+				        setTimeout(function(){
+				        	$(self).addClass('rotateInUpRight')
+				        }, 400);
+		        	});
+			    };
 		        return{
 		        	makeCall:makeCall,
 		        	errorHtml:errorHtml,
 		        }
 		}(this, $));
-
-	   	$( ".like" ).on("click", function(event){
-	    	event.preventDefault()
-	    	var defered = ajax_caller.makeCall.call(this);
-	    	var self = this;
-	        defered.fail(function(response){
-	        	$('#popup1 .content').html(ajax_caller.errorHtml({error: response.responseJSON.error ||response.responseJSON.detail , error_code: response.responseJSON.error_code ||response.responseJSON.status_code}));
-	        	$('#popup1').show()
-	        });
-	        defered.done(function(response) {
-	        	debugger;
-		        $(self).prev().val(response.total_likes);
-		        $(self).toggleClass('down');
-		        $(self).find('i.fa').toggleClass('shake');
-		        $(self).find('i.fa').toggleClass('fa-thumbs-o-up').toggleClass('fa-thumbs-up')
-		        setTimeout(function(){
-		        	$(self).addClass('shake')
-		        }, 400);
-
-	        });
-	    });
-//  for login button iteraction
-	    $('.login-button').click(function(){
-	            animate.breakGlass();
-	            setTimeout(function(){
-	                 $('#wrap').css('display', 'none')
-	             },800);
-	        });
 	    $('.close').click(function(){
 	    	$(this).parent().parent().hide()
 	    });
-    // end of login interaction
+    
+    var like_list = $('.like');
+
+    _.each(like_list, function(item){
+    	$(item).on('click', _.bind(ajax_caller.makeCall, item))
+    });
 });
