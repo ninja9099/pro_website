9cff2ca5e096f374e74324ab4298f3babc002db5
pankaj9099 <kamanipankaj9099@gmail.com>
2017-11-14 09:28:38 +0530

    fixed the like comment box

--- blog/static/src/js/article.js
+++ blog/static/src/js/article.js
@@ -5,11 +5,22 @@ require(['config'], function(){
 		$( ".login-popup a" ).hover(function() {
     			$( '.popup' ).show();
   			});
-  		// $('.popup').mouseleave(function() {
-   	//  			$('.popup').hide();
-  		// 	});
 		$('#close').click(function(){
 			$('.popup').hide();
+		});
+
+		var fixmeTop = $('.fixme').offset().top;       // get initial position of the element
+
+		$(window).scroll(function() {                  // assign scroll event listener
+
+		    var currentScroll = $(window).scrollTop() - 210; // get current position
+
+		    if (currentScroll >= fixmeTop) {           // apply position: fixed if you
+		        $('.fixme').addClass('scrollfix bounceInDown animated');
+
+		    } else {                                   // apply position: static
+		        $('.fixme').removeClass('scrollfix bounceInDown animated');
+		    }
 		});
 	})
 });
