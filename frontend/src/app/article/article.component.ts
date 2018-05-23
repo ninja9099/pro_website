44076d9fd04409386f560116db037cfce2c5f843
pankaj <kamanipankaj9099@gmail.com>
2018-05-23 18:04:37 +0530

    changed navbar component html structure

--- frontend/src/app/article/article.component.ts
+++ frontend/src/app/article/article.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get('id');
   }
 
 }
