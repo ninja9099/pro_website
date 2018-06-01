aa3a93b40f6a618fff9c314f82407d513d9b7208
Pankaj Kamani <kamanipankaj9099@gmail.com>
2018-06-01 09:27:46 +0530

    Added service to componennt

--- frontend/src/app/article/article.component.ts
+++ frontend/src/app/article/article.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GetArticlesService } from './get-articles.service';
 @Component({
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
 
-  constructor(private _route: ActivatedRoute) { }
+  constructor(private _route: ActivatedRoute, private _ArticleService: GetArticlesService) { }
 
   ngOnInit() {
     const id = +this._route.snapshot.paramMap.get('id');
--- frontend/src/app/article/get-articles.service.ts
+++ frontend/src/app/article/get-articles.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+// tslint:disable-next-line:import-blacklist
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable()
 export class GetArticlesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+   }
+  articleUrl = 'http://127.0.0.1:8000/api/v1/article/';
+
+  getArticle() {
+    return this.http.get(this.articleUrl);
+  }
 
 }
