5181e8f8ff73253a8de63c4f5783cfb7b3423aa1
Pankaj Kamani <kamanipankaj9099@gmail.com>
2018-07-16 08:00:36 +0530

    added the showcase module for display html pages

---
+++ frontend/src/app/showcase/showcase.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ShowcaseRoutingModule } from './showcase-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ShowcaseRoutingModule
+  ],
+  declarations: []
+})
+export class ShowcaseModule { }
