165717fd14b90cb0e9c42096bfd2e6fef7338b95
Pankaj Kamani <Pankaj.K@simformsolutions.comPankaj.K@simformsolutions.comPankaj.K@simformsolutions.com>
2018-08-06 19:01:07 +0530

    added and designed article list page

--- frontend/src/app/article/articlelist/articlelist.component.ts
+++ frontend/src/app/article/articlelist/articlelist.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../_services/api.service';
 import { GlobalVars } from '../../app.component';
+import { DomSanitizer } from '@angular/platform-browser';
 
 
 @Component({
@@ -9,11 +10,14 @@ import { GlobalVars } from '../../app.component';
   styleUrls: ['./articlelist.component.css']
 })
 export class ArticleListComponent implements OnInit {
-  private artilce_list: any[];
+  private article_list: any[];
   private data: any[];
   public context: string = localStorage.getItem('context');
 
-  constructor(private _route: ActivatedRoute, private _ApiService: ApiService, public _gvars: GlobalVars) { 
+  constructor(private _route: ActivatedRoute,
+    private _ApiService: ApiService,
+    public _gvars: GlobalVars,
+  ) {
     _gvars.context = 'blog';
    }
   ngOnInit() {
@@ -24,7 +28,7 @@ export class ArticleListComponent implements OnInit {
     this._ApiService.getArticles(null, limit).subscribe((data: Array<object>) => {
       // tslint:disable-next-line:no-debugger
       debugger;
-      this.artilce_list = data['objects'];
+      this.article_list = data['objects'];
       console.log(data);
     });
   }
